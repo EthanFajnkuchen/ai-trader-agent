4b34c6639cc246197d2e21823e5baff684c4453e
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2024-03-02 15:04:18 +0200

    Implement sentimental analysis model

---
+++ Models/sentiment_analysis.py
@@ -0,0 +1,29 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from typing import Tuple 
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+tokenizer = AutoTokenizer.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+model = AutoModelForSequenceClassification.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis").to(device)
+labels = ["negative", "neutral", "positive"]
+
+
+def estimate_sentiment(news):
+    if news:
+        tokens = tokenizer(news, return_tensors="pt", padding=True).to(device)
+
+        result = model(tokens["input_ids"], attention_mask=tokens["attention_mask"])[
+            "logits"
+        ]
+        result = torch.nn.functional.softmax(torch.sum(result, 0), dim=-1)
+        probability = result[torch.argmax(result)]
+        sentiment = labels[torch.argmax(result)]
+        return probability, sentiment
+    else:
+        return 0, labels[-1]
+
+
+if __name__ == "__main__":
+    tensor, sentiment = estimate_sentiment(["Why everyone should buy AAPL stocks now!"])
+    print(tensor, sentiment)
+    print(torch.cuda.is_available())
