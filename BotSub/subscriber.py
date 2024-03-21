8787a9bd144bdb48726c38ee336d78ee210717a5
jeremzan <135054175+jeremzan@users.noreply.github.com>
2024-03-21 12:51:53 +0200

    Fixed the timer and the decoding for channel
    -No need to if cases in bot suscriber  because we can do it from trader agent already
    - When stopping the session manually or automatically with timer we kill the task so timer won't run
    - In the case the timer stop we need to use channel to send stopping message

--- BotSub/subscriber.py
+++ BotSub/subscriber.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv("./../.env")
 
 # Configuration de la connexion Redis
-redis_client = redis.Redis(host='redis', port=6379, db=0)
+redis_client = redis.StrictRedis(host="redis", port=6379, charset="utf-8", decode_responses=True)
 pubsub = redis_client.pubsub()
 
 # Souscrire au canal Redis
@@ -20,15 +20,14 @@ bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_TOKEN"))
 def listen_for_trades():
     for message in pubsub.listen():
         if message['type'] == 'message':
-            # Extraire les informations de trade du message
+            # The message data is already decoded, thanks to decode_responses=True
             trade_info = message['data']
-            trade_info = message['data'].decode('utf-8')  # Decode from bytes to string
             print(trade_info)
             print(type(trade_info))
             
             parts = trade_info.split(': ')
             chat_id = parts[1]
-            message_info = parts[0] + ' 💸'
+            message_info = parts[0]
 
             # Envoyer une notification via Telegram
             bot.send_message(chat_id=chat_id, text=message_info)
